dcae00e7728384aebc940c59244bed8e72b0f8a6
Lindor linor <lindorlinor@gmail.com>
2026-05-11 12:04:25 +0200

    aggiunta parte iniziale del tutorial

---
+++ appuntini.md
@@ -0,0 +1,11 @@
+# Appuntini
+
+- il file index.js dentro a src deve stare l' perchè è l'entry point di tutto, il resto può essere spostato secondo le nostre preferenze
+
+- script e dependencies dentro a package.json come in angular alla fine
+- il file lock invece è uno snapshot di tutto il dependency tree
+
+- togliamo un po di boilerplate code
+- puoi usare jsx per scrivere elemnti del dom html, altrimenti puoi usae createElement
+- le funzioni possono ritornare un singolo componente html, quindi si wrappa tutto introrno ad un div se serve che vengano ritorante piu componenti
+- fragment per unire piu elementi da ritornare e non vuoi wrapparli in altri nodi (div, section etc) usare altri nodi
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function Gretting() {
+    return (<React.Fragment>
+                <Componente />
+                <P />
+                <Input />
+            </React.Fragment>)
+            
+}
+        
+{/* ricordati di chiudere sempre i tag che sono singoli */}
+const Componente = () => <h1 className='io-sono-una-classe'>Ciao sono un componente carino carino</h1> //qui non posso avere logica aggiuntiva, quello che scrivo è quello che ritorno
+const P = () => {
+    //posso avere logica aggiuntiva perchè ho messo le parentesi graffe e il return
+    return <p>io sono qui solo per vedere come raggruppare elementi html senza aggiungere altri nodi (div)</p>
+}
+const Input = () => <input type="text" placeholder='scrivi qualcosa' />; 
+
+
+
+
+// function Gretting() {
+//     return React.createElement('h2',{}, 'Ciao sono un componente carino carino')
+// }
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<Gretting />); //ricorda di chiudere sempre il componente
